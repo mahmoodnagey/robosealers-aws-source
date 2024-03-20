3a51986358dcad207f0b85662ee53eef33b19018
mahmoodnagey <121254803+mahmoodnagey@users.noreply.github.com>
2024-03-20 02:55:56 +0200

    Delete server directory to upload updated one with no bugs

--- server/routes/org/record.route.js
+++
@@ -1,8 +0,0 @@
-const app = require("express").Router();
-const recordController = require("../../controllers/org/record.controller")
-
-app.get("/list", recordController.listRecords);
-app.get("/get", recordController.getRecord);
-
-
-module.exports = app
